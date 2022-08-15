86a877ac89b9afaaa83d04a43d510070c487a186
ABCD <ABCD@DESKTOP-F9TKGNL>
2022-08-15 23:50:39 +0530

    comment on forgot password module

--- studentatt/src/main/java/com/student/controller/ForgotPassController.java
+++ studentatt/src/main/java/com/student/controller/ForgotPassController.java
@@ -2,22 +2,30 @@ package com.student.controller;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.student.service.EmailService;
+
 @Controller
 public class ForgotPassController {
 	Random random = new Random(1000);
+	
+	@Autowired
+	private EmailService emailService;
 
 	// email id form open handler
-	@RequestMapping("/forgot")
-	public String openEmailForm() {
-		return "forgot_email_form";
-	}
+//	@RequestMapping("/forgot")
+//	public String openEmailForm() {
+//		return "forgot_email_form";
+//	}
 
 	@RequestMapping("/send-otp")
-	public String sendOTP(@RequestParam("username") String email) {
+	public String sendOTP(@RequestParam("username") String email, HttpSession session) {
 		System.out.println("Email: " + email);
 
 		// generating 4 digit otp
@@ -25,7 +33,18 @@ public class ForgotPassController {
 		System.out.println("OTP: " + otp);
 
 		// write code for send otp to email
+		String subject = "OTP From SCM";
+		String message = "<h1> OTP = " +otp+ "</h1>";	
+		String to = email;
+		boolean flag = this.emailService.sendEmail(subject, message, to);
+		
+		if(flag) {
+			return "verify_otp";
+		}else {
+			session.setAttribute("message", "Check your email id !!");
+			return "forgot_email_form";
+		}
 
-		return "verify_otp";
+		
 	}
 }
